cc433852d93a7aea82555e4fa20c78b17ed69288
Victor Villegas <vgarza01@hotmail.com>
2021-10-27 18:05:13 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector:'app-root',
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+  titulo: string = 'Contador App';
+  numero: number = 10;
+  base  : number = 5 ;
+
+  acumular( valor: number){
+     this.numero += valor
+  }
+
+
 }
